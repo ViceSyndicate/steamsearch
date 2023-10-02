4f1e2666aef9d870e5eee58e8d6440219eab2dc8
Victor S <victor.skarerverket@gmail.com>
2023-10-02 13:26:16 +0200

    renamed credentials and updated gitignore

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
 def main():
     print("Hello World!")
     searchQuery = input("Search: ")
 
     gc = gspread.service_account()
+    
     
 
 if __name__ == "__main__":
