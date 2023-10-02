83651e986dff1530d28d6024be38c854f7733c87
Victor S <victor.skarerverket@gmail.com>
2023-10-02 18:36:52 +0200

    Added comments, explaining code.

--- main.py
+++ main.py
@@ -3,15 +3,23 @@ import gspread
 def main():
     print("Executing...")
     print('------------')
+    # get get authentification for spreadsheets
     gc = gspread.service_account(filename='credentials.json')
     gc.auth
+    #get testspreadsheet
     spreadsheet = gc.open('TestSheet')
+    #get working sheet
     worksheet = spreadsheet.worksheet('Sheet1')
+    #get all values from column 1
     gameList = worksheet.col_values(1)
 
     for val in gameList:
         print(val)
-
+    
+    # TODO do steam api search on each title in gameList
+    # get Rating, Tags and store them for each game.
+    # Update sheet columns 2 and 3 with those values. 
+    
     print('------------')
     searchQuery = input("Press Enter to continue")
 
