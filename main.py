54286e029c33db7f44381ecae0d5fed8a86066e9
Victor S <victor.skarerverket@gmail.com>
2023-10-02 14:31:15 +0200

    Getting all values from A1.

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import gspread
-from oauth2client.service_account import ServiceAccountCredentials
 
 def main():
-    print("Hello World!")
-    searchQuery = input("Search: ")
+    print("Executing...")
+    print('------------')
+    gc = gspread.service_account(filename='credentials.json')
+    gc.auth
+    spreadsheet = gc.open('TestSheet')
+    worksheet = spreadsheet.worksheet('Sheet1')
+    gameList = worksheet.col_values(1)
 
-    gc = gspread.service_account()
-    
-    
+    for val in gameList:
+        print(val)
+
+    print('------------')
+    searchQuery = input("Press Enter to continue")
 
 if __name__ == "__main__":
     main()
