d80b146d1d49b6fc0e85a6424b761eae18d9a035
Victor S <victor.skarerverket@gmail.com>
2023-10-09 15:10:48 +0200

    Testrun: exporting test data to .csv file

--- apiRequests.py
+++ apiRequests.py
@@ -1,13 +1,110 @@
 import requests
 import json
+import time
+
+class Game:
+    def __init__(self, name, ID, platforms, genres, short_Description):
+        self.name = name
+        self.ID = ID
+        self.platforms = platforms
+        self.genres = genres
+        self.short_Description = short_Description
 
 steamGamesUrl = 'http://api.steampowered.com/ISteamApps/GetAppList/v2/'
+steamGameIds = []
 steamGameReviewScore = 'https://store.steampowered.com/appreviews/{id}?json=1'
 
+#def getRequest(url):
+
+def getRequest(url):
+    request = requests.get(url)
+    if request.status_code != 200:
+        print('GET ' + url + ' Failed!')
+        print('returning empty string...')
+        return ''
+    else:
+        print('Success! Returning Content...')
+        gamesData = json.loads(request.content)
+        return gamesData
+
+    #jsonData = json.loads(jsonGames.content)
+    #return jsonData
+
 def getGameList():
-    jsonGames = requests.get(steamGamesUrl)
-    jsonData = json.loads(jsonGames.content)
+    jsonData = getRequest(steamGamesUrl)
     return jsonData['applist']['apps']
 
-def compareGameLists():
-    print('WIP')
+def returnMatchingGameIds(sheetGames, steamGames):
+    # For every gameName in sheetGames
+    for sheetName in sheetGames:
+        #print(sheetName)
+        # Check every gameName in steamGames
+        for game in steamGames:
+            # if a steamGame has a title matching our list of games
+            # Add that games appid to our steamGamesId list
+            if game['name'] == sheetName:
+                #print(game['name'])
+                #print(sheetName)
+                if not steamGameIds.__contains__(game['appid']):
+                    steamGameIds.append(game['appid'])
+                else:
+                    print("couldn't find " + game['name'])
+    return steamGameIds
+
+def getAppData(id):
+    url = "https://store.steampowered.com/api/appdetails?appids=" + str(id)
+    jsonGame = requests.get(url)
+    jsonData = json.loads(jsonGame.content)
+    success = jsonData[str(id)]['success']
+    gameData = {}
+    if (success):
+        name = jsonData[str(id)]['data']['name']
+
+        #loop through each item in generes and get description
+        genres_data  = jsonData[str(id)]['data']['genres']
+        gameGenres = [genre['description'] for genre in genres_data]
+        #platforms = jsonData[str(id)]['data']['platforms']
+
+        platforms = []
+        platformData = jsonData[str(id)]['data']['platforms']
+        # {'windows': True, 'mac': True, 'linux': False}
+        for platform, value in platformData.items():
+            # if the platform is true: add it to list of platforms.
+            # platforms ex: "windows": true, "mac": true,"linux": false
+            if value:
+                platforms.append(platform)
+
+        short_Description = jsonData[str(id)]['data']['short_description']
+        time.sleep(2.5)
+        reviewScore = getReviewData(id)
+
+        gameData = {
+            "name": name,
+            "id": id,
+            "platforms": platforms,
+            "genres": gameGenres,
+            "reviewScore": reviewScore,
+            "short_Description": short_Description
+        }
+        return gameData
+        
+    else:
+        print("couldn't get appdetails!")
+    #return jsonData[str(id)]['applist']['apps']
+
+    # Rating, Platform, description(Single/Multiplayer capabilities) and Tags. 
+def getReviewData(id): 
+    url = 'https://store.steampowered.com/appreviews/' + str(id) + '?json=1'
+    review_score = ''
+    jsonGame = requests.get(url)
+    jsonData = json.loads(jsonGame.content)
+    if (jsonData['success']):
+        review_score = jsonData['query_summary']['review_score_desc']
+    else:
+        print("Couldn't get appreview data")
+    return review_score
+
+# Rating, Platform, description(Single/Multiplayer capabilities) and Tags. 
+
+# TODO 
+# Fill google sheet fields with the data that we DID find.
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import gspread
 import apiRequests
+import time
+import csv
 
 def main():
     print("Executing...")
@@ -12,17 +14,9 @@ def main():
     #get working sheet
     worksheet = spreadsheet.worksheet('Copy of Keys for Giveaway')
     #get all values from column 1
-    gameList = worksheet.col_values(2)
+    sheetGameList = worksheet.col_values(2)
     #remove the first value which is the name of the column.
-    gameList.pop(0)
-
-    '''
-    counter = 1
-    for val in gameList:
-
-        print(str(counter) + ' - ' + val)
-        counter = counter +1
-    '''
+    sheetGameList.pop(0)
 
     # TODO Make steam api search on each title in gameList
     # get Rating, Platform and Tags. 
@@ -31,7 +25,56 @@ def main():
     # Update sheet columns 2, 3 and 4 with those values for each game.
 
     
+    
+    
     steamGames = apiRequests.getGameList()
+    matchingIds = apiRequests.returnMatchingGameIds(sheetGameList, steamGames)
+    
+    matchingIds = matchingIds[:5]
+
+
+    #apiRequests.getAppData(matchingIds[0])
+    print('matchingames: '+str(len(matchingIds))+'/'+str(len(sheetGameList)))
+    gameDataList = []
+    
+    for id in matchingIds:
+        # wait 2.5s to avoid spamming steams api 
+        # and getting timed out/blocked.
+        time.sleep(2.5)
+        print('Requesting data on ID: ' + str(id))
+        gameDataList.append(apiRequests.getAppData(id))
+    
+    print(gameDataList)
+    print(gameDataList[0])
+
+    
+    # Specify the CSV file name
+    csv_filename = "games.csv"
+
+    # Open the CSV file for writing
+    with open(csv_filename, 'w', newline='') as csvfile:
+        # Create a CSV writer object
+        csv_writer = csv.writer(csvfile)
+
+        # Write the header row (if needed)
+        header = ["Name", "ID", "Platforms", "Genres", "Description"]
+        csv_writer.writerow(header)
+
+        # Write data for each Game object in the list
+        for game in gameDataList:
+            # Extract the attributes from the Game object
+            row_data = [
+            game['name'],
+            game['id'],
+            ', '.join(game['platforms']),
+            ', '.join(game['genres']),
+            game['short_Description']
+            ]
+
+            # Write the row to the CSV file
+            csv_writer.writerow(row_data)
+
+        print(f"Data has been written to {csv_filename}")
     
     print('------------')
     pause = input("Press Enter to continue")
